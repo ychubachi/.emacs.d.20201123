d595bd87513e1c252a68284c26f83973ba6d4340
Yoshihide Chubachi <yoshi@chubachi.net>
2012-03-11 00:50:02 +0900

    README

--- init.el
+++ init.el
@@ -45,17 +45,6 @@
 ;; Uniquify
 (require 'uniquify)
 
-;; Evernote
-(require 'evernote-mode)
-(setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8"))
-(global-set-key "\C-cec" 'evernote-create-note)
-     (global-set-key "\C-ceo" 'evernote-open-note)
-     (global-set-key "\C-ces" 'evernote-search-notes)
-     (global-set-key "\C-ceS" 'evernote-do-saved-search)
-     (global-set-key "\C-cew" 'evernote-write-note)
-     (global-set-key "\C-cep" 'evernote-post-region)
-     (global-set-key "\C-ceb" 'evernote-browser)
-
 ;; japanese-anthy をデフォルトの input-method にする。
 (load-library "anthy")
 (setq default-input-method "japanese-anthy")
