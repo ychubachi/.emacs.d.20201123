5b96a7a51683f2600cca121ac4766457f7870ecb
Yoshihide Chubachi <yoshi@chubachi.net>
2012-03-12 00:53:52 +0900

    mozc

--- init.el
+++ init.el
@@ -23,10 +23,18 @@
 (autoload 'scss-mode "scss-mode")
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
 
-;; japanese-anthy をデフォルトの input-method にする。
-(load-library "anthy")
-(setq default-input-method "japanese-anthy")
-(setq anthy-wide-space " ")
+;; ibus-mode
+;; http://d.hatena.ne.jp/supermassiveblackhole/20100609/1276059762
+(require 'ibus)
+; Turn on ibus-mode automatically after loading .emacs
+(add-hook 'after-init-hook 'ibus-mode-on)
+; Use C-SPC for Set Mark command
+(ibus-define-common-key ?\C-\s nil)
+; Use C-/ for Undo command
+(ibus-define-common-key ?\C-/ nil)
+; Change cursor color depending on IBus status
+(setq ibus-cursor-color '("limegreen" "white" "blue"))
+(global-set-key "\C-\\" 'ibus-toggle)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Options (Run M-x customize-option to change them)
