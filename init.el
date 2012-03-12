67dc3665961f4ac3f54dc87468bfd63345796ca3
Yoshihide Chubachi <yoshi@chubachi.net>
2012-03-13 03:13:45 +0900

    check

--- init.el
+++ init.el
@@ -43,5 +43,12 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
  '(show-paren-mode t)
  '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
