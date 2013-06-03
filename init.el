4363a27de7b08f4c9e90594084bf18ed047e3582
Yoshihide Chubachi <yoshi@chubachi.net>
2013-06-03 23:31:26 +0900

    el-getによるパッケージの管理など，大改造

--- init.el
+++ init.el
@@ -1,67 +1,30 @@
-;; Load path
-(let ((default-directory  "~/.emacs.d/lisp/"))
-  (normal-top-level-add-subdirs-to-load-path))
+;; load paths
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+	      (expand-file-name (concat user-emacs-directory path))))
+	(add-to-list 'load-path default-directory)
+	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+	    (normal-top-level-add-subdirs-to-load-path))))))
+(add-to-load-path "elisp" "git")
 
-;; shell 
-(global-set-key (kbd "C-z") 'shell)
-(ansi-color-for-comint-mode-on) ; color (for Shell) 要らないかな?
+;; ;; Emacs
+;; (setq inhibit-startup-screen t)
+;; (tool-bar-mode 0)
+;; (scroll-bar-mode 0)
+;; (menu-bar-mode 0)
+;; (setq frame-title-format
+;;       (format "%%f - Emacs@%s" (system-name)))
 
-;; Magit
-(require 'magit)
-(global-set-key (kbd "M-v") 'magit-status)
+;; (setq show-paren-delay 0)
+;; (show-paren-mode t)
+;; (setq show-paren-style 'expression)
+;; (set-face-background 'show-paren-match-face nil)
+;; (set-face-underline-p 'show-paren-match-face "green")
 
-;; haml
-(setq auto-mode-alist
-      (cons (cons "\\.haml$" 'haml-mode) auto-mode-alist))
-(autoload 'haml-mode "haml-mode.el" "haml-mode" t)
-(add-hook 'haml-mode-hook
-	  '(lambda ()
-	     (setq indent-tabs-mode nil)
-	     (define-key haml-mode-map "\C-m" 'newline-and-indent)))
+;; ;; key bindings
+;; (keyboard-translate ?\C-h ?\C-?)
+;; (global-set-key (kbd "C-x ?") 'help-command)
 
-;; scss
-(autoload 'scss-mode "scss-mode")
-(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
-
-;; ibus-mode
-;; http://d.hatena.ne.jp/supermassiveblackhole/20100609/1276059762
-;; (require 'ibus)
-;; ; Turn on ibus-mode automatically after loading .emacs
-;; (add-hook 'after-init-hook 'ibus-mode-on)
-;; ; Use C-SPC for Set Mark command
-;; (ibus-define-common-key ?\C-\s nil)
-;; ; Use C-/ for Undo command
-;; (ibus-define-common-key ?\C-/ nil)
-;; ; Change cursor color depending on IBus status
-;; (setq ibus-cursor-color '("limegreen" "white" "blue"))
-;; ; Use C-\ for IBus toggle command
-;; (global-set-key "\C-\\" 'ibus-toggle)
-
-;; flymake
-(require 'flymake)
-(global-set-key [f3] 'flymake-display-err-menu-for-current-line)
-(global-set-key [f4] 'flymake-goto-next-error)
-
-;; yatex
-(require 'yatex)
-(setq auto-mode-alist
-      (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
-(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Options (Run M-x customize-option to change them)
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(rst-mode-hook (quote (flymake-find-file-hook)))
- '(show-paren-mode t)
- '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Ricty")))))
+(require 'emacs-kicker)
