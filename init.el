f188a3a4677bfcef8f8b32c88baaf1c5584a2449
Yoshihide Chubachi <yoshi@chubachi.net>
2013-06-04 12:56:57 +0900

    21-term.el

--- init.el
+++ init.el
@@ -44,20 +44,6 @@
 (global-auto-revert-mode 1)
 
 
-;; If you do use M-x term, you will notice there's line mode that acts like
-;; emacs buffers, and there's the default char mode that will send your
-;; input char-by-char, so that curses application see each of your key
-;; strokes.
-;;
-;; The default way to toggle between them is C-c C-j and C-c C-k, let's
-;; better use just one key to do the same.
-(require 'term)
-(define-key term-raw-map  (kbd "C-'") 'term-line-mode)
-(define-key term-mode-map (kbd "C-'") 'term-char-mode)
-
-;; Have C-y act as usual in term-mode, to avoid C-' C-y C-'
-;; Well the real default would be C-c C-j C-y C-c C-k.
-(define-key term-raw-map  (kbd "C-y") 'term-paste)
 
 ;; use ido for minibuffer completion
 (require 'ido)
