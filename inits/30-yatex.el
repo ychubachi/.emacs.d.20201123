302bd473ff368ad700fa5c2351b9529f683bda77
Yoshihide Chubachi <Yoshihide-chubachi+github@gmail.com>
2013-06-04 21:48:48 +0900

    yatexÌdbusievinceAgj֘A𕪗£µ܂µ½B

--- inits/30-yatex.el
+++ inits/30-yatex.el
@@ -77,29 +77,3 @@
 (add-hook 'yatex-mode-hook
           '(lambda ()
              (define-key YaTeX-mode-map (kbd "C-c e") 'evince-forward-search)))
-
-;; http://oku.edu.mie-u.ac.jp/~okumura/texwiki/?Emacs#v19f2543
-(require 'dbus)
-
-(defun un-urlify (fname-or-url)
-  "A trivial function that replaces a prefix of file:/// with just /."
-  (if (string= (substring fname-or-url 0 8) "file:///")
-      (substring fname-or-url 7)
-    fname-or-url))
-
-(defun evince-inverse-search (file linecol &rest ignored)
-  (let* ((fname (un-urlify file))
-         (buf (find-file fname))
-         (line (car linecol))
-         (col (cadr linecol)))
-    (if (null buf)
-        (message "[Synctex]: %s is not opened..." fname)
-      (switch-to-buffer buf)
-      (goto-line (car linecol))
-      (unless (= col -1)
-        (move-to-column col)))))
-
-(dbus-register-signal
- :session nil "/org/gnome/evince/Window/0"
- "org.gnome.evince.Window" "SyncSource"
- 'evince-inverse-search)
