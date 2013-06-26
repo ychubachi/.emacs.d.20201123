eff19c36f0c3bb4834889930254d606bf9aab6c6
Yoshihide Chubachi <yoshi@chubachi.net>
2013-06-26 21:16:40 +0900

    packageの整理作業で，まずはmagitのみ設定．

--- inits/20-package.el
+++ inits/20-package.el
@@ -3,45 +3,45 @@
 ;; ここに使っているパッケージを書く。
 (defvar installing-package-list
   '(
-    auto-complete
-    coffee-mode
-    color-theme		                ; nice looking emacs
-;    color-theme-tango	                ; check out color-theme-solarized
-    dropdown-list
-    escreen            			; screen for emacs, C-\ C-h
-    flymake-easy
-    flymake-php
-    flymake-ruby
-    flymake-haml
-    flymake-sass
-    google-c-style
-    haml-mode
-    haskell-mode
-    js2-mode
+;;     auto-complete
+;;     coffee-mode
+;;     color-theme		                ; nice looking emacs
+;; ;    color-theme-tango	                ; check out color-theme-solarized
+;;     dropdown-list
+;;     escreen            			; screen for emacs, C-\ C-h
+;;     flymake-easy
+;;     flymake-php
+;;     flymake-ruby
+;;     flymake-haml
+;;     flymake-sass
+;;     google-c-style
+;;     haml-mode
+;;     haskell-mode
+;;     js2-mode
     magit
-    magithub
-    markdown-mode
-    migemo
-    open-junk-file
-    php-mode
-    rbenv
-    recentf-ext
-    redo+
-    ruby-block
-    ruby-electric
-    scala-mode
-    scss-mode
-    switch-window			; takes over C-x o
-    yaml-mode
-    yasnippet
-    zencoding-mode			; http://www.emacswiki.org/emacs/ZenCoding
-    smex
-    goto-last-change
-    buffer-move
-    mew
-    multi-term
-    shell-pop
-    w3m
+;;     magithub
+;;     markdown-mode
+;;     migemo
+;;     open-junk-file
+;;     php-mode
+;;     rbenv
+;;     recentf-ext
+;;     redo+
+;;     ruby-block
+;;     ruby-electric
+;;     scala-mode
+;;     scss-mode
+;;     switch-window			; takes over C-x o
+;;     yaml-mode
+;;     yasnippet
+;;     zencoding-mode			; http://www.emacswiki.org/emacs/ZenCoding
+;;     smex
+;;     goto-last-change
+;;     buffer-move
+;;     mew
+;;     multi-term
+;;     shell-pop
+;;     w3m
     ))
 
 (let ((not-installed (loop for x in installing-package-list
