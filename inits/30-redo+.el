10197f36c3166e06f3757bbb64f8955cbf56df2c
Yoshihide Chubachi <yoshi@chubachi.net>
2013-06-28 01:49:23 +0900

    キーの割り当てを修正しました．

--- inits/30-redo+.el
+++ inits/30-redo+.el
@@ -6,7 +6,7 @@
     (package-install package)))
 
 (require 'redo+)
-(global-set-key (kbd "C-'") 'redo)
+(global-set-key (kbd "C-,") 'redo)
 
 ;; ついでにundoも設定
-(global-set-key (kbd "C-,") 'undo)
+(global-set-key (kbd "C-.") 'undo)
