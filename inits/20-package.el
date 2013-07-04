72c3533a97d42f93c477bec527bf00ca2060eeb1
Yoshihide Chubachi <yoshi@chubachi.net>
2013-07-05 00:26:37 +0900

    内容を削除しました．

--- inits/20-package.el
+++ inits/20-package.el
@@ -1,35 +1,4 @@
-;; (require 'cl)
+;; 20-package.el
+;;
+;; パッケージのインストールはそれぞれのファイルで行うようにしました．
 
-;; ;; ここに使っているパッケージを書く。
-;; (defvar installing-package-list
-;;   '(
-;; ;;     auctex
-;; ;;     color-theme		                ; nice looking emacs
-;; ;;     dropdown-list
-;; ;;     escreen            			; screen for emacs, C-\ C-h
-;; ;;     flymake-easy
-;; ;;     js2-mode
-;; ;;     magithub
-;; ;;     migemo
-;; ;;     open-junk-file
-;; ;;     php-mode
-;; ;;     recentf-ext
-;; ;;     redo+
-;; ;;     switch-window			; takes over C-x o
-;; ;;     yaml-mode
-;; ;;     yasnippet
-;; ;;     zencoding-mode			; http://www.emacswiki.org/emacs/ZenCoding
-;; ;;     smex
-;; ;;     goto-last-change
-;; ;;     buffer-move
-;; ;;     multi-term
-;; ;;     shell-pop
-;;     ))
-
-;; (let ((not-installed (loop for x in installing-package-list
-;;                             when (not (package-installed-p x))
-;;                             collect x)))
-;;   (when not-installed
-;;     (package-refresh-contents)
-;;     (dolist (pkg not-installed)
-;;         (package-install pkg))))
