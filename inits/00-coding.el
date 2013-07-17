17ef4230869b4fe42be86e043584d86bb60f4ddb
vagrant <vagrant@precise>
2013-07-17 15:24:43 +0000

    Add more coding-systems

--- inits/00-coding.el
+++ inits/00-coding.el
@@ -1,4 +1,7 @@
-(set-language-environment "Japanese")
+(set-language-environment "japanese")
 (prefer-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(setq default-buffer-file-coding-systems 'utf-8)
