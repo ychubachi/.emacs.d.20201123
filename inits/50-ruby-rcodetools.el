2eafd2d6c407362e3aef27b2086016272da53144
Yoshihide Chubachi <yoshi@chubachi.net>
2013-07-21 20:50:30 +0900

    rcodetoolsを強化しました

--- inits/50-ruby-rcodetools.el
+++ inits/50-ruby-rcodetools.el
@@ -3,6 +3,17 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (add-to-list 'load-path "~/.rbenv/versions/2.0.0-p195/lib/ruby/gems/2.0.0/gems/rcodetools-0.8.5.0")
-(require 'rcodetools)
-(define-key ruby-mode-map (kbd "<C-return>") 'rct-complete-symbol)
 
+;; rcodetools
+(require 'rcodetools)
+(setq rct-find-tag-if-available nil)
+(defun ruby-mode-hook-rcodetools ()
+  (define-key ruby-mode-map (kbd "<C-return>") 'rct-complete-symbol)
+  (define-key ruby-mode-map "\M-\C-i" 'rct-complete-symbol)
+  (define-key ruby-mode-map "\C-c\C-t" 'ruby-toggle-buffer)
+  (define-key ruby-mode-map "\C-c\C-d" 'xmp)
+  (define-key ruby-mode-map "\C-c\C-f" 'rct-ri))
+(add-hook 'ruby-mode-hook 'ruby-mode-hook-rcodetools)
+
+(setq rct-get-all-methods-command "PAGER=cat fri -l")
+;; See docs
