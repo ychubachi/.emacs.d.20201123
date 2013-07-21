32f443d5914a18c6d6df8d0f330167f99e294bca
Yoshihide Chubachi <yoshi@chubachi.net>
2013-07-21 20:50:04 +0900

    emacs-lisp-modeを追加しました

---
+++ inits/10-emacs-lisp-mode.el
@@ -0,0 +1,17 @@
+(add-hook 'emacs-lisp-mode-hook
+	  (lambda ()
+	    ;; Use spaces, not tabs.
+	    (setq indent-tabs-mode nil)
+	    ;; Keep M-TAB for `completion-at-point'
+	    (define-key flyspell-mode-map "\M-\t" nil)
+	    ;; Pretty-print eval'd expressions.
+	    (define-key emacs-lisp-mode-map
+	      "\C-x\C-e" 'pp-eval-last-sexp)
+	    ;; Recompile if .elc exists.
+	    (add-hook (make-local-variable 'after-save-hook)
+		      (lambda ()
+			(byte-force-recompile default-directory)))
+	    (define-key emacs-lisp-mode-map
+	      "\r" 'reindent-then-newline-and-indent)))
+(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode) ;; Requires Ispell
