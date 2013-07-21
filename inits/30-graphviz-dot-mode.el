302fbdb3e9087e500a4c172857923c418469a5d7
Yoshihide Chubachi <yoshi@chubachi.net>
2013-07-21 20:05:17 +0900

    graphviz-dot-modeを追加しました

---
+++ inits/30-graphviz-dot-mode.el
@@ -0,0 +1,9 @@
+;; パッケージのインストール
+(setq package-list '(graphviz-dot-mode))
+(dolist (package package-list)
+  (when (not (package-installed-p package))
+    (package-install package)))
+
+(setq auto-mode-alist
+      (append '(("\\.dot$" . graphviz-dot-mode)
+
