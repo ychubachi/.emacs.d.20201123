b0587f931e72f7c359591047f9bc6dd878e8feec
Yoshihide Chubachi <yoshi@chubachi.net>
2013-07-24 15:00:41 +0900

    helmを設定しなおしました

---
+++ inits/30-helm.el
@@ -0,0 +1,40 @@
+;; パッケージのインストール
+(setq package-list '(helm helm-descbinds migemo helm-migemo))
+(dolist (package package-list)
+  (when (not (package-installed-p package))
+    (package-install package)))
+
+;;
+;; http://www49.atwiki.jp/ntemacs/m/pages/32.html
+;;
+(require 'helm-config)
+;(require 'helm-gtags)
+
+(helm-mode 1)
+
+
+(global-set-key (kbd "C-c h") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+;; 候補を作って描写するまでのタイムラグ設定する（デフォルトは 0.1）
+; (setq helm-idle-delay 0.2)
+
+;; C-h でバックスペースと同じように文字を削除できるようにする
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+
+;; ミニバッファで C-k 入力時にカーソル以降を削除する
+(setq helm-delete-minibuffer-contents-from-point t)
+
+;; TAB で補完する
+(define-key helm-map (kbd "TAB") 'helm-execute-persistent-action)
+
+;; describe-bindings(C-h b)のhelm版
+(require 'helm-descbinds)
+(helm-descbinds-mode)
+
+;; C-o は ime変換用として使っているので、helm-next-source を C-l に変更する
+(define-key helm-map (kbd "C-o") nil)
+(define-key helm-map (kbd "C-l") 'helm-next-source)
+
+;; helm-migemo
+(require 'helm-migemo)
