0746ee56ac45c0132752519da63cf6517940b990
Yoshihide Chubachi <yoshi@chubachi.net>
2013-07-24 16:24:01 +0900

    helmの修正

--- inits/30-helm.el
+++ inits/30-helm.el
@@ -1,5 +1,7 @@
+;; https://github.com/emacs-helm/helm/wiki
+
 ;; パッケージのインストール
-(setq package-list '(helm helm-descbinds migemo helm-migemo))
+(setq package-list '(helm helm-descbinds migemo helm-migemo imenu-anywhere))
 (dolist (package package-list)
   (when (not (package-installed-p package))
     (package-install package)))
@@ -12,29 +14,32 @@
 
 (helm-mode 1)
 
-
 (global-set-key (kbd "C-c h") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 ;; 候補を作って描写するまでのタイムラグ設定する（デフォルトは 0.1）
 ; (setq helm-idle-delay 0.2)
 
-;; C-h でバックスペースと同じように文字を削除できるようにする
-(define-key helm-map (kbd "C-h") 'delete-backward-char)
-
 ;; ミニバッファで C-k 入力時にカーソル以降を削除する
 (setq helm-delete-minibuffer-contents-from-point t)
 
-;; TAB で補完する
+;; TAB で補完する（C-zと同じにする）
 (define-key helm-map (kbd "TAB") 'helm-execute-persistent-action)
 
-;; describe-bindings(C-h b)のhelm版
-(require 'helm-descbinds)
-(helm-descbinds-mode)
+;; C-h でバックスペースと同じように文字を削除できるようにする
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 
 ;; C-o は ime変換用として使っているので、helm-next-source を C-l に変更する
 (define-key helm-map (kbd "C-o") nil)
 (define-key helm-map (kbd "C-l") 'helm-next-source)
 
+;; describe-bindings(C-h b)のhelm版
+(require 'helm-descbinds)
+(helm-descbinds-mode)
+
 ;; helm-migemo
 (require 'helm-migemo)
+
+;(global-set-key (kbd "C-.") 'imenu-anywhere)
+
