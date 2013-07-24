d613bd9da7b9ce536eecb42f9f83de0dd1a9b2fb
Yoshihide Chubachi <yoshi@chubachi.net>
2013-07-24 22:50:07 +0900

    Remove comments.

--- inits/10-appearance.el
+++ inits/10-appearance.el
@@ -4,8 +4,6 @@
 
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
-;; (tool-bar-mode -1)			; no tool bar with icons
-;; (scroll-bar-mode -1)			; no scroll bars
 (menu-bar-mode 0)
 (setq frame-title-format
       (format "%%f - Emacs@%s" (system-name)))
