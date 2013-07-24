2c0d632b4de1f05ee5bbc223c2b90ada6c485845
Yoshihide Chubachi <yoshi@chubachi.net>
2013-07-24 15:03:44 +0900

    ミニバッファでDELがきかないので，translate方式に変更してみました
    漢字変換中のDELがうまくいかないのであきらめました．

--- inits/00-keybind.el
+++ inits/00-keybind.el
@@ -1,6 +1,11 @@
 ;; key bindings
+
+;; C-hをBSにする
 (global-set-key "\C-h" 'delete-backward-char)
+; (keyboard-translate ?\C-h ?\C-?)
+
 (global-set-key (kbd "C-c C-h") 'help-command)
+
 (global-set-key (kbd "C-z") 'shell)
 
 
