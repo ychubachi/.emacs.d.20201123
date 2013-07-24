5c6b9c18d8c16599e0679ae73dd7e58bd5820e88
Yoshihide Chubachi <yoshi@chubachi.net>
2013-07-25 00:44:58 +0900

    Customizeの出力先をcustom.elにしました．

--- init.el
+++ init.el
@@ -1,8 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Author:	Y.Chubachi （中鉢 欣秀）
 ;; Created:	2013-06-04
-;;
-;; Thanks: http://qiita.com/items/5f1cd86e2522fd3384a0
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; My minimum settings
@@ -14,11 +12,7 @@
 ;; Load a lot of packages
 (load "~/.emacs.d/load.el")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(shell-pop-shell-type (quote ("multi-term" "*terminal<1>*" (quote (lambda nil (multi-term))))))
- '(shell-pop-universal-key "C-z"))
+;; Custom file
+(setq custom-file "~/.emacs.d/custom.el")
+(if (file-exists-p (expand-file-name "~/.emacs.d/custom.el"))
+    (load (expand-file-name custom-file) t nil nil))
