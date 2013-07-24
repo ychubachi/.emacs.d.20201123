8bacbf472db8cbb9c8420406a5ab95b5bca072af
Yoshihide Chubachi <yoshi@chubachi.net>
2013-07-25 01:12:01 +0900

    Customize theme

--- custom.el
+++ custom.el
@@ -8,6 +8,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+ '(custom-enabled-themes (quote (manoj-dark)))
  '(shell-pop-shell-type (quote ("multi-term" "*terminal<1>*" (quote (lambda nil (multi-term))))))
  '(shell-pop-universal-key "C-z"))
 (custom-set-faces
