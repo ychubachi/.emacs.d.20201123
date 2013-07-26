190bd72aee99c2442f7a7662b1e53b3a4746d8c1
Yoshihide Chubachi <yoshi@chubachi.net>
2013-07-26 21:51:36 +0900

    F-1,2,3,4を設定しました

--- function.el
+++ function.el
@@ -10,10 +10,30 @@
 		       (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
 (global-set-key [f11] 'my/fullscreen)
 
-(defun my/open-inits-folder()
-  "自分の設定フォルダを開きます．"
+(defun my/open-init-folder()
+  "設定フォルダを開きます．"
   (interactive)
   (dired "~/.emacs.d/"))
-(global-set-key (kbd "C-`") 'my/open-inits-folder)
+(global-set-key (kbd "<f1>") 'my/open-init-folder)
+
+(defun my/open-note()
+  "備忘録を開きます．"
+  (interactive)
+  (find-file "~/Dropbox/Note/index.org"))
+(global-set-key (kbd "<f2>") 'my/open-note)
+  
+(defun my/open-todo()
+  "備忘録を開きます．"
+  (interactive)
+  (find-file "~/Dropbox/Todo/todo.txt"))
+(global-set-key (kbd "<f3>") 'my/open-todo)
+
+(defun my/open-project-folder()
+  "プロジェクトフォルダを開きます．"
+  (interactive)
+  (dired "~/git/"))
+(global-set-key (kbd "<f4>") 'my/open-project-folder)
+
+
 
 
