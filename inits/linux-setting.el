e0f94154c2658efa04d98c1e46a403b932f3f8e2
Yoshihide Chubachi <yoshi@chubachi.net>
2013-11-17 22:59:13 +0900

    Set overlay mode for mozc.el

--- inits/linux-setting.el
+++ inits/linux-setting.el
@@ -25,6 +25,7 @@
 (require 'mozc)
 (setq default-input-method "japanese-mozc")
 (global-set-key (kbd "C-o") 'toggle-input-method)
+(setq mozc-candidate-style 'overlay)
 
 ;; ================================================================
 ;; Font設定
