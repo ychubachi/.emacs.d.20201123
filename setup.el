ebeb7c7a2437afcf4fcb1710906782e86a776fa4
Yoshihide Chubachi <yoshi@chubachi.net>
2013-12-26 18:55:02 +0900

    Update

--- setup.el
+++ setup.el
@@ -1,10 +1,4 @@
 
-;; ================================================================
-;; パッケージの初期設定
-;; - パッケージをインストールするディレクトリの設定
-;; - ダウンロードするリポジトリの設定
-;; - 必要に応じてアーカイブ
-;; ================================================================
 (require 'package)
 (setq package-user-dir "~/.emacs.d/packages/")
 (setq package-archives '(("gnu" .
@@ -21,23 +15,11 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; second block
-
-;; ================================================================
-;; load pathの設定
-;; ================================================================
 (let ((default-directory "~/.emacs.d/site-lisp/"))
   (normal-top-level-add-subdirs-to-load-path))
 
-;; ================================================================
-;; 日本語の設定
-;; ================================================================
 (set-language-environment "japanese")
 (prefer-coding-system 'utf-8)
-
-;; ================================================================
-;; Backupの設定
-;; ================================================================
 
 ;; create backup file in ~/.emacs.d/backup
 (setq make-backup-files t)
@@ -49,19 +31,12 @@
 (setq auto-save-file-name-transforms
       `((".*" ,(expand-file-name "~/.emacs.d/backup/") t)))
 
-;; ================================================================
-;; init-loaderの設定
-;; - init-loaderのインストール
-;; ================================================================
 (when (not (package-installed-p 'init-loader))
   (package-install 'init-loader))
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits")
 ; (setq init-loader-show-log-after-init nil)
 
-;; ================================================================
-;; Custom file
-;; ================================================================
 (setq custom-file "~/.emacs.d/custom.el")
 (if (file-exists-p (expand-file-name "~/.emacs.d/custom.el"))
     (load (expand-file-name custom-file) t nil nil))
