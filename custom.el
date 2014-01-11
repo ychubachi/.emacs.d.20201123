255e76b58f9249e1e16e892b30c0ac3f99f8623c
Yoshihide Chubachi <yoshi@chubachi.net>
2014-01-11 17:55:03 +0900

    Macの斜体問題を解決しました

--- custom.el
+++ custom.el
@@ -64,4 +64,4 @@ Entered on %U
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(font-lock-comment-face ((t (:foreground "chocolate1" :slant normal)))))
