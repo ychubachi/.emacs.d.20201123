9e0cec2c4d0d140f67f59322b533ece10a985eb7
Yoshihide Chubachi <yoshi@chubachi.net>
2014-01-16 21:28:06 +0900

    古いorg-modeの設定により，C-c C-oの挙動が変だったので修正

--- init.el
+++ init.el
@@ -166,10 +166,10 @@
 
 (cond ((eq system-type 'gnu/linux)
        (setq org-latex-pdf-process '("latexmk -e '$latex=q/platex %S/' -e '$bibtex=q/pbibtex %B/' -e '$makeindex=q/mendex -o %D %S/' -e '$dvipdf=q/dvipdfmx -o %D %S/' -norc -gg -pdfdvi %f"))
-       (setq org-file-apps '(("pdf" . "evince %s"))))
+       )
       ((eq system-type 'darwin)
        (setq org-latex-pdf-process '("latexmk -e '$latex=q/platex %S/' -e '$bibtex=q/pbibtex %B/' -e '$makeindex=q/mendex -o %D %S/' -e '$dvipdf=q/dvipdfmx -o %D %S/' -norc -gg -pdfdvi %f"))
-       (setq org-file-apps '(("pdf" . "/usr/bin/open -a Skim %s")))))
+       ))
 
 ;; jsarticle
 (add-to-list 'org-latex-classes
