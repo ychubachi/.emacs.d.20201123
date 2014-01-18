f812c2423b8e148faedf08e7d28ff9ab9cf0b1e3
Yoshihide Chubachi <yoshi@chubachi.net>
2014-01-18 22:05:18 +0900

    Add SOMEDAY

--- custom.el
+++ custom.el
@@ -42,7 +42,7 @@ Entered on %U
  '(org-mobile-inbox-for-pull "~/Dropbox/Note/from-mobile.org")
  '(org-src-fontify-natively t)
  '(org-startup-with-inline-images t)
- '(org-todo-keywords (quote ((sequence "TODO" "WAITING" "DONE"))))
+ '(org-todo-keywords (quote ((sequence "TODO" "WAITING" "|" "DONE" "SOMEDAY"))))
  '(org2blog/wp-use-sourcecode-shortcode t)
  '(outline-minor-mode-prefix "")
  '(package-archives (quote (("org" . "http://orgmode.org/elpa/") ("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/"))))
