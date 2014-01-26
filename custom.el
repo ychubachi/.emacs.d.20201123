427d726a02269bbc105d0ac6e029a22408325fdf
Yoshihide Chubachi <yoshi@chubachi.net>
2014-01-27 00:25:31 +0900

    column titleの設定

--- custom.el
+++ custom.el
@@ -20,6 +20,7 @@
  '(custom-enabled-themes (quote (manoj-dark)))
  '(custom-safe-themes (quote ("fc6e906a0e6ead5747ab2e7c5838166f7350b958d82e410257aeeb2820e8a07a" default)))
  '(debug-on-error t)
+ '(default-frame-alist (quote ((font . "ricty-13.5"))))
  '(display-time-mode t)
  '(fci-rule-color "#383838")
  '(graphviz-dot-preview-extension "pdf")
@@ -68,4 +69,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:foreground "chocolate1" :slant normal)))))
+ '(font-lock-comment-face ((t (:foreground "chocolate1" :slant normal))))
+ '(org-column-title ((t (:background "grey30" :underline t :weight bold :height 135)))))
