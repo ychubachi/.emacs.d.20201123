cf62af15506c274b08aa1a2bdc15f349264c05c8
Yoshihide Chubachi <yoshi@chubachi.net>
2014-04-28 17:38:23 +0900

    mu4e

--- init.el
+++ init.el
@@ -575,12 +575,10 @@ SCHEDULED: %t
     (require 'mu4e)
     (require 'org-mu4e)
 
-    ;; defaults
-
     (setq mu4e-maildir       "~/Maildir")
-    (setq mu4e-drafts-folder "/[Gmail].下書き")
-    (setq mu4e-sent-folder   "/[Gmail].送信済みメール")
-    (setq mu4e-trash-folder  "/[Gmail].ゴミ箱")
+    (setq mu4e-drafts-folder "/[Gmail].Drafts")
+    (setq mu4e-trash-folder  "/[Gmail].Trash")
+    (setq mu4e-sent-folder   "/[Gmail].All Mail")
 
     ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
     (setq mu4e-sent-messages-behavior 'delete)
@@ -589,27 +587,17 @@ SCHEDULED: %t
     ;; you can quickly switch to your Inbox -- press ``ji''
     ;; then, when you want archive some messages, move them to
     ;; the 'All Mail' folder by pressing ``ma''.
-
     (setq mu4e-maildir-shortcuts
-          '( ("/INBOX"                  . ?i)
-             ("/[Gmail].スター付き"     . ?*)
-             ("/[Gmail].送信済みメール" . ?s)
-             ("/[Gmail].下書き"         . ?d)
-             ("/[Gmail].すべてのメール" . ?a)
-             ("/[Gmail].ゴミ箱"         . ?t)
-             ("/[Gmail].迷惑メール"     . ?j)))
+          '( ("/INBOX"             . ?i)
+             ("/[Gmail].All Mail"  . ?a)
+             ("/[Gmail].Drafts"    . ?d)
+             ("/[Gmail].Trash"     . ?t)))
 
     ;; allow for updating mail using 'U' in the main view:
     ;; I have this running in the background anyway
     (setq mu4e-get-mail-command "offlineimap")
 
-    ;; sending mail -- replace USERNAME with your gmail username
-    ;; also, make sure the gnutls command line utils are installed
-    ;; package 'gnutls-bin' in Debian/Ubuntu
-
     (require 'smtpmail)
-
-    ;; alternatively, for emacs-24 you can use:
     (setq message-send-mail-function 'smtpmail-send-it
           smtpmail-stream-type 'starttls
           smtpmail-default-smtp-server "smtp.gmail.com"
@@ -626,29 +614,18 @@ SCHEDULED: %t
     (when (fboundp 'imagemagick-register-types)
       (imagemagick-register-types))
 
-;;; message view action
-    (defun mu4e-msgv-action-view-in-browser (msg)
-      "View the body of the message in a web browser."
-      (interactive)
-      (let ((html (mu4e-msg-field (mu4e-message-at-point t) :body-html))
-            (tmpfile (format "%s/%d.html" temporary-file-directory (random))))
-        (unless html (error "No html part for this message"))
-        (with-temp-file tmpfile
-          (insert
-           "<html>"
-           "<head><meta http-equiv=\"content-type\""
-           "content=\"text/html;charset=UTF-8\">"
-           html))
-        (browse-url (concat "file://" tmpfile))))
+    (setq mu4e-msg2pdf "/usr/bin/msg2pdf")
 
     (add-to-list 'mu4e-view-actions
-                 '("View in browser" . mu4e-msgv-action-view-in-browser) t)
+                 '("View in browser" . mu4e-action-view-in-browser) t)
 
     ;; convert org mode to HTML automatically
     (setq org-mu4e-convert-to-html t)
 
     ;; need this to convert some e-mails properly
     (setq mu4e-html2text-command "html2text -utf8 -width 72")
+
+    (add-to-list 'mu4e-bookmarks '("flag:flagged" "Starred" ?s))
     )
   (mu4e)
   )
