95e8ea56e0c43bcc7ede6c64a8f0430abc73eb67
Yoshihide Chubachi <yoshi@chubachi.net>
2014-05-08 22:37:29 +0900

    Make shell-pop as a plugin.

--- init.el
+++ init.el
@@ -655,14 +655,6 @@ SCHEDULED: %t
 
 (add-hook 'emacs-lisp-mode-hook 'outline-minor-mode)
 
-(my/package-install-and-require 'shell-pop)
-
-(custom-set-variables
- '(shell-pop-autocd-to-working-dir nil)
- '(shell-pop-shell-type (quote ("eshell" "*eshell*" (lambda nil (eshell)))))
- '(shell-pop-universal-key "C-z")
- '(shell-pop-window-height 30))
-
 (dolist (package '(undo-tree))
   (when (not (package-installed-p package))
     (package-install package)))
