b24d708986d2e7dc60cdf331d311cfce728247d4
Yoshihide Chubachi <Yoshihide-chubachi+github@gmail.com>
2014-05-16 01:03:05 +0900

    Clean up

--- plugins-enabled/shell-pop.el
+++
@@ -1,8 +0,0 @@
-
-(my/package-install-and-require 'shell-pop)
-
-(custom-set-variables
- '(shell-pop-autocd-to-working-dir nil)
- '(shell-pop-shell-type (quote ("eshell" "*eshell*" (lambda nil (eshell)))))
- '(shell-pop-universal-key "C-z")
- '(shell-pop-window-height 30))
