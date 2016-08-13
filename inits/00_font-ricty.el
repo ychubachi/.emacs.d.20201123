4679221a948070af03c1998f3696aeed51702d70
Yoshihide Chubachi <yoshi@chubachi.net>
2016-08-13 09:34:06 +0900

    Make font_ricty.el

---
+++ inits/00_font-ricty.el
@@ -0,0 +1,16 @@
+;;; デフォルトフォントの設定
+
+;; ↓のテーブルが揃っていればOK
+;;  |あぱ　ああ|
+;;  |+-+-+-+-+-|
+;;  |imimimimim|
+
+;; ｰ Fontに関する調査
+;;   - [[file:test.org::*Emacs%E3%81%AE%E3%83%95%E3%82%A9%E3%83%B3%E3%83%88%E3%81%AE%E3%81%8A%E8%A9%B1][Emacsのフォントのお話]]
+;;   - この値はcustomize可能です．
+
+(cond
+ ((eq system-type 'gnu/linux)
+  (add-to-list 'default-frame-alist '(font . "ricty-13.5")))
+ ((eq system-type 'darwin)
+  (add-to-list 'default-frame-alist '(font . "ricty-14"))))
