97c6697f9d2c1f3676fd38f925ee2080443a5164
Yoshihide Chubachi <yoshi@chubachi.net>
2016-08-21 12:47:06 +0900

    Update mozc

--- inits/linux-mozc-popup.el
+++ inits/linux-mozc-popup.el
@@ -1,7 +1,10 @@
-(use-package mozc-popup
-  :if (eq system-type 'gnu/linux)
-  :init
-  (progn
-    (setq default-input-method "japanese-mozc")
-    (setq mozc-candidate-style 'popup))
-  :ensure t)
+;;; mozc
+
+;; .Xresource
+;; emacs.useXIM:false
+
+;; xrdb -merge ~/.Xresource
+
+(require 'mozc-popup)
+(setq default-input-method "japanese-mozc")
+(setq mozc-candidate-style 'popup)
