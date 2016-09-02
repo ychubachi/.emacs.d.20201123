8a284e64a9e880aea7c674dd68b883955042ba54
Yoshihide Chubachi <yoshi@chubachi.net>
2016-09-03 01:17:46 +0900

    Typo

--- inits/90_my_function.el
+++ inits/90_my_function.el
@@ -1,6 +1,6 @@
 ;;; F12でWikiを開く
 (defun my/open-wiki-home()
-   "設定フォルダを開きます．"
+   "Wikiフォルダを開きます．"
    (interactive)
    (find-file "~/Dropbox/Org/Wiki/home.org"))
 (bind-key "<f12>" 'my/open-wiki-home)
