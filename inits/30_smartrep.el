ee48d779bd6afae408f2cd50e35bfd00aeb0cbca
Yoshihide Chubachi <yoshi@chubachi.net>
2016-09-11 19:01:03 +0900

    Tweak

--- inits/30_smartrep.el
+++ inits/30_smartrep.el
@@ -1,5 +1,6 @@
 ;;; smartrep
 
-;; - [[http://sheephead.homelinux.org/2011/12/19/6930/][連続操作を素敵にするsmartrep.el作った - sheephead]]
+;; - 連続操作を素敵にするsmartrep.el作った - sheephead
+;;   http://sheephead.homelinux.org/2011/12/19/6930/
 
 (use-package smartrep :ensure t)
