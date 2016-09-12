ea864fb5c7d72e1c12684aa738bae6bd1b43d5ab
Yoshihide Chubachi <yoshi@chubachi.net>
2016-09-13 07:07:19 +0900

    Update shell-pop

--- inits/50_shell-pop.el
+++ inits/50_shell-pop.el
@@ -1,10 +1,10 @@
 ;;; shell-pop
 (use-package shell-pop
-  :init
+  :bind ("C-z" . shell-pop)
+  :config
   (custom-set-variables
    '(shell-pop-autocd-to-working-dir nil)
    '(shell-pop-shell-type
      (quote ("eshell" "*eshell*" (lambda nil (eshell)))))
-   '(shell-pop-universal-key "C-z")
    '(shell-pop-window-height 30))
   :ensure t)
