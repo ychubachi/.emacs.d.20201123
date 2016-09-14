7d843d7e6b7eb0f79fc708847e1de5e670bbbc2c
Yoshihide Chubachi <yoshi@chubachi.net>
2016-09-14 11:50:33 +0900

    Add a function

--- inits/90_my_function.el
+++ inits/90_my_function.el
@@ -1,6 +1,13 @@
-;;; F12でWikiを開く
-(defun my/open-wiki-home()
-   "Wikiフォルダを開きます．"
+;;; F5でnotes.orgを開く
+(defun my/open-notes-org()
+   "note.orgを開きます．"
    (interactive)
-   (find-file "~/Dropbox/Org/Wiki/home.org"))
-(bind-key "<f12>" 'my/open-wiki-home)
+   (find-file "~/Dropbox/Org/notes.org"))
+(bind-key "<f5>" 'my/open-notes-org)
+
+;;; F6でindex.orgを開く
+(defun my/open-index-org()
+   "index.orgを開きます．"
+   (interactive)
+   (find-file "~/git/www_chubachi_net/org/index.org"))
+(bind-key "<f6>" 'my/open-index-org)
