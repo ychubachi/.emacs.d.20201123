c586f5b0d9c1ad52677e5dd1cebe21c74e0c1339
Yoshihide Chubachi <yoshi@chubachi.net>
2016-09-23 13:27:02 +0900

    Edit region-bindings-mode

--- inits/30_multiple-cursors.el
+++
@@ -1,6 +0,0 @@
-;;; multiple-cursors
-
-;; 複数のカーソルを扱う
-;; - [[https://github.com/magnars/multiple-cursors.el][magnars/multiple-cursors.el]]
-
-(use-package multiple-cursors :ensure t)
--- inits/50_region-bindings-mode.el
+++ inits/50_region-bindings-mode.el
@@ -1,10 +1,9 @@
-;;; region-bindings-mode
+;;; リージョンがある間のキーバインディングを変更する
+(use-package multiple-cursors)
 
-;; リージョンがある間のキーバインディングを変更する
 (use-package region-bindings-mode
   :config
   (progn
-    (region-bindings-mode-enable)
     (bind-keys :map region-bindings-mode-map
 	       ;; multiple-cursors
 	       ("a" . mc/mark-all-like-this)
@@ -13,5 +12,5 @@
 	       ("m" . mc/mark-more-like-this-extended)
 	       ("e" . mc/edit-lines)
 	       ;; aign-regexp
-	       ("r" . align-regexp)))
-  :ensure t)
+	       ("r" . align-regexp))
+    (region-bindings-mode-enable)))
