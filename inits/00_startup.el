8e02e04c73a4ff41dbcd95b63cb69e60578437d7
Yoshihide Chubachi <yoshi@chubachi.net>
2016-09-28 01:40:23 +0900

    Revert

--- inits/00_startup.el
+++ inits/00_startup.el
@@ -22,8 +22,8 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-;; (package-initialize)
-(package-refresh-contents)
+(package-initialize)
+;; (package-refresh-contents)
 
 ;;; use-packageの導入
 
