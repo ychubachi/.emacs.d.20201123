133a48f783d5e4d731aab494218101de8d113340
Yoshihide Chubachi <yoshi@chubachi.net>
2016-10-05 21:33:59 +0900

    Clean up comments

--- inits/00_startup.el
+++ inits/00_startup.el
@@ -1,11 +1,5 @@
 ;;; サーバの開始
 
-;; - Emacs serverの開始
-;; - Emacs 既に起動している場合は立ち上げない
-;;   - [[http://stackoverflow.com/questions/5570451/how-to-start-emacs-server-only-if-it-is-not-started][configuration - How to start emacs server only if it is not started? - Stack Overflow]]
-
-;; WindowsでEmacsにテキストファイルを関連付けする - 技術memo - http://nenono.hatenablog.com/entry/2015/01/05/155225
-
 (require 'server)
 (when (and (>= emacs-major-version 23)
            (equal window-system 'w32))
