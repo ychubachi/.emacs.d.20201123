36fdfc500bd19ded73fddcf644ff3cd2bab50729
Yoshihide Chubachi <yoshi@chubachi.net>
2016-10-05 22:28:26 +0900

    Disable server-kill-buffer-query-function
    - it couse some problem using org-ref

--- inits/00_startup.el
+++ inits/00_startup.el
@@ -1,18 +1,23 @@
 ;;; サーバの開始
-
 (require 'server)
+
+;; Suppress error "directory ~/.emacs.d/server is unsafe" on windows.
 (when (and (>= emacs-major-version 23)
            (equal window-system 'w32))
   (defun server-ensure-safe-dir (dir) "Noop" t))
-  ; Suppress error "directory ~/.emacs.d/server is unsafe" on windows.
+
 (unless (server-running-p)
   (server-start))
+
+;; バッファをキルするときに出る確認を抑制
 (remove-hook
   'kill-buffer-query-functions
   'server-kill-buffer-query-function)
 
-;;; packageシステムの初期化
+;; emacsclientでサーバに接続時，上の設定が無効になることの対処
+(defun server-kill-buffer-query-function () t)
 
+;;; packageシステムの初期化
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
