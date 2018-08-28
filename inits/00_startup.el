d9f1e78ef5a20a0884563574306bf1a4202e6d13
Yoshihide Chubachi <yoshi@chubachi.net>
2018-08-28 17:06:25 +0900

    C-h と font

--- inits/00_startup.el
+++ inits/00_startup.el
@@ -38,47 +38,10 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;;; C-hをDELと交換
-
-;; (global-set-key (kbd "C-h") 'delete-backward-char) はものたりない
-;; なぜかは忘れた
-
-;; - C-h が押されたら，C-? (<DEL>) に変換する．
-;; - ヘルプは[F1]でも参照できる．
-;; - 参考
-;; 	 - [[http://akisute3.hatenablog.com/entry/20120318/1332059326][EmacsのC-hをbackspaceとして使用する - 勉強日記]]
-;; 	 - [[http://www.gnu.org/software/emacs/manual/html_node/efaq/Swapping-keys.html#Swapping-keys][Swapping keys - GNU Emacs FAQ]]
-;; - keyboad-translate関数はサーバにする際動作しなかった
-;; 	 - [[http://lists.gnu.org/archive/html/help-gnu-emacs/2009-10/msg00505.html][Re: keyboard-translate not working with emacs daemon]]
-
-(define-key key-translation-map [?\C-h] [?\C-?])
-
-;;;; C-c ? を help-for-help にします
-(bind-key "C-c ?" 'help-for-help)
-
 ;;; 日本語/UTF-8に
 ;; - 言語環境を日本語に，コード体系をUTF-8にします．
 (set-language-environment "japanese")
 (prefer-coding-system 'utf-8)
-
-;;; デフォルトフォントの設定
-
-;; ↓のテーブルが揃っていればOK
-;;  |あぱ　ああ|
-;;  |+-+-+-+-+-|
-;;  |imimimimim|
-
-;; ｰ Fontに関する調査
-;;   - [[file:test.org::*Emacs%E3%81%AE%E3%83%95%E3%82%A9%E3%83%B3%E3%83%88%E3%81%AE%E3%81%8A%E8%A9%B1][Emacsのフォントのお話]]
-;;   - この値はcustomize可能です．
-
-(cond
- ((eq system-type 'gnu/linux)
-  (add-to-list 'default-frame-alist '(font . "ricty-13.5")))
- ((eq system-type 'darwin)
-  (add-to-list 'default-frame-alist '(font . "ricty-14")))
- ((eq system-type 'windows-nt)
-  (add-to-list 'default-frame-alist '(font . "Ricty Diminished"))))
 
 ;;; 無駄な行末の空白を削除
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
---
+++ inits/10_ctrl-h.el
@@ -0,0 +1,17 @@
+;;; C-hをDELと交換
+
+;; (global-set-key (kbd "C-h") 'delete-backward-char) はものたりない
+;; なぜかは忘れた
+
+;; - C-h が押されたら，C-? (<DEL>) に変換する．
+;; - ヘルプは[F1]でも参照できる．
+;; - 参考
+;; 	 - [[http://akisute3.hatenablog.com/entry/20120318/1332059326][EmacsのC-hをbackspaceとして使用する - 勉強日記]]
+;; 	 - [[http://www.gnu.org/software/emacs/manual/html_node/efaq/Swapping-keys.html#Swapping-keys][Swapping keys - GNU Emacs FAQ]]
+;; - keyboad-translate関数はサーバにする際動作しなかった
+;; 	 - [[http://lists.gnu.org/archive/html/help-gnu-emacs/2009-10/msg00505.html][Re: keyboard-translate not working with emacs daemon]]
+
+(define-key key-translation-map [?\C-h] [?\C-?])
+
+;;;; C-c ? を help-for-help にします
+(bind-key "C-c ?" 'help-for-help)
